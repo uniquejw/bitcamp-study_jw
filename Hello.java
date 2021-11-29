755e633d5b739b44535b616781b5d0e011d05c75
uniquejw <jaewongogo@naver.com>
2021-11-29 09:37:38 +0900

    day3

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Hello{
     public static void main(String[] args){
         System.out.println("Hello world!");
-        // abcdef다  :
-        // 가나다    :
+        // abcdef다  : abc
+        // 가나다    : bcad
         // a가나다   :
         // abc가나다 :
         // 
